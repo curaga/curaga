0e9928c864366d18a7d15f9cef1342e804940dd1
Wei-Meng Lee <weimeng@staticfusion.org>
2020-05-31 17:49:22 +0800

    Add log out button

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'static#index'
 
   scope path: '-' do
+    devise_for :users
     resources :docs, except: :show
   end
 
