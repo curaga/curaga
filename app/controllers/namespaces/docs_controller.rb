c2df0244ef6bad7457ca3b3d8c3e849e1a4b5d19
Wei-Meng Lee <weimeng@staticfusion.org>
2020-06-16 02:15:16 +0800

    Always redirect namespace to root if default

--- app/controllers/namespaces/docs_controller.rb
+++ app/controllers/namespaces/docs_controller.rb
@@ -6,6 +6,8 @@ module Namespaces
     include Pundit
 
     def show
+      return redirect_to root_doc_url(params[:id]) if @namespace.id.to_s == ENV.fetch('DEFAULT_NAMESPACE_ID')
+
       @document = @namespace.documents.friendly.find(params[:id])
     end
   end
