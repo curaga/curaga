28b7158cd14a8e8c75810d35e903497996a446c8
Wei-Meng Lee <weimeng@staticfusion.org>
2020-06-19 15:16:02 +0800

    Initial implementation of titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # rubocop:disable Rails/HelperInstanceVariable
+  def page_title(*titles)
+    @page_title ||= []
+
+    @page_title.push(*titles.compact) if titles.any?
+
+    @page_title.join(' · ')
+  end
+  # rubocop:enable Rails/HelperInstanceVariable
 end
