b3a9aeda13943bec8a086f5ac1f78bfb110861f9
Wei-Meng Lee <weimeng@staticfusion.org>
2020-06-28 17:24:39 +0800

    Add undo/redo functionality

--- app/javascript/prosemirror/state.js
+++ app/javascript/prosemirror/state.js
@@ -1,5 +1,6 @@
 import { EditorState } from 'prosemirror-state';
 import { keymap } from 'prosemirror-keymap';
+import { history } from 'prosemirror-history';
 import { baseKeymap } from 'prosemirror-commands';
 import { buildKeymap } from './keymap';
 import schema from './schema';
@@ -9,6 +10,7 @@ import { linkPaste } from './plugins/linkPaste';
 export let state = EditorState.create({
   schema,
   plugins: [
+    history(),
     inputRules,
     keymap(buildKeymap(schema)),
     keymap(baseKeymap),
