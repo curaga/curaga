ca586766cf2c210b4560cbcc37285be8fef43039
Wei-Meng Lee <weimeng@staticfusion.org>
2020-07-04 15:40:01 +0800

    Initial work on mobile-friendly doc layout

--- app/javascript/packs/document.js
+++ app/javascript/packs/document.js
@@ -3,6 +3,12 @@ import Document from '../document.svelte'
 document.addEventListener('turbolinks:load', () => loadDocument());
 
 function loadDocument() {
+  const namespaceMenuButton = document.querySelector('.js__namespace-menu-btn');
+  const namespaceMenu = document.querySelector('.js__namespace-menu');
+  namespaceMenuButton.addEventListener('click', (event) => {
+    namespaceMenu.classList.toggle('hidden');
+  });
+
   const el = document.querySelector('#curaga-document');
 
   if(typeof(el) != 'undefined' && el != null) {
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '192': '48rem',
+      },
+    },
+  },
+};
